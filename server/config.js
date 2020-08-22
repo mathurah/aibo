25da1a5d965574bb97451c1268e38cdd2991b776
mathurahravigulan <mathurah.ravigulan@edu.uwaterloo.ca>
2020-08-22 15:09:19 -0400

    Initial commit

---
+++ server/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  twilio: {
+    accountSid: 'ACf9d5edfdae6e981002bb894762cf2baf',
+    apiKey: 'SK143167b86915d94c24efa66435088dbd',
+    apiSecret: 'uTnxcIfYofx82VwXoIQU8EielYMiaL9J'
+  }
+};
+
+//keep private!!
