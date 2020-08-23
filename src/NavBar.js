73c6734c4e746b96c043f0143c722f719749c9c8
juliaturner073 <60072112+juliaturner073@users.noreply.github.com>
2020-08-23 01:06:46 -0400

    Add files via upload

---
+++ src/NavBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import logo from "./images/logo2.png";
+
+const Example = () => {
+  return (
+    <div class="topnav">
+      <div class="left">
+        <a class="active" href="#home">
+          <img src={logo} alt="Girl in a jacket" />
+        </a>
+        <a href="#news">Video Chat</a>
+        <a href="#contact">Matches</a>
+        <a href="#about">Dashboard</a>
+        <a href="#about">Task Tracker</a>
+      </div>
+      <div class="right">
+        <a class="login" href="#news">
+          Log In
+        </a>
+        <a class="signup" href="#contact">
+          <button class="buttonvibe">Try for free!</button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Example;
